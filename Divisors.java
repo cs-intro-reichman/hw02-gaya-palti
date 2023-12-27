0b369d97efb7613fbfc9019f3d89bcbf9f3a06be
gaya-palti <gaya.palti@post.runi.ac.il>
2023-12-27 15:28:10 +0200

    Solve Divisors exercise

--- Divisors.java
+++ Divisors.java
@@ -3,6 +3,12 @@
  */
 public class Divisors {
 	public static void main (String[] args) {
-		//// Put your code here
+		int x = Integer.parseInt(args[0]);
+		int divisors = 1;
+		while (divisors < x){
+		//System.out.println("d " + d);
+		if ( x % divisors == 0) {System.out.println(divisors);}
+        divisors++;
 	}
+  }
 }
