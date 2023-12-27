175bf09ffce270526a98050870ff991c385df265
gaya-palti <gaya.palti@post.runi.ac.il>
2023-12-27 21:35:08 +0200

    Update DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -8,10 +8,14 @@ public class DamkaBoard {
 			if (row % 2 == 1) {
 				System.out.print(" ");
 			}
-			for (int col = 0; col < n; col++){
+			for (int col = 0; col < n - 1; col++){
 				System.out.print("* ");
 			}
-			System.out.println("");
+			if (row % 2 == 0) {
+				System.out.println("* ");
+			} else {
+				System.out.println("*");
+			}
 		}
   	}
 }
