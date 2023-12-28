13827553574a933a50f964ee8b6be1f71aaed287
gaya-palti <gaya.palti@post.runi.ac.il>
2023-12-28 17:51:45 +0200

    One Of each stats 1

--- OneOfEachStats1.java
+++ OneOfEachStats1.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  *  Computes some statistics about families in which the parents decide 
  *  to have children until they have at least one child of each gender.
@@ -6,6 +8,40 @@
  */
 public class OneOfEachStats1 {
 	public static void main (String[] args) {
-		//// Put your code here
+		int T = Integer.parseInt(args[0]);
+		int seed = Integer.parseInt(args[1]); // Initailizes a random numbers generator with the given seed value
+        Random generator = new Random(seed);  	
+		int child2 = 0;	
+		int child3 = 0;	
+		int child4 = 0;	
+		int maxChild = 0;
+		double children = 0;
+
+		for (int i = 0; i < T; i++){
+			boolean girl = false;
+			boolean boy = false;
+			int childInFam = 0;
+			while (!boy || !girl){
+				double rand = generator.nextDouble();
+				if (rand <= 0.5) girl = true;
+				else boy = true;
+				children ++;
+				childInFam ++;
+			}
+			if (childInFam == 2) child2 ++; 
+			else if (childInFam == 3) child3 ++;
+			else if (childInFam == 4) child4 ++;
+		}
+
+		maxChild = (Math.max(child2 , Math.max(child3 , child4)));
+		if (maxChild == child2) maxChild = 2;
+		else if (maxChild == child3) maxChild = 3;
+		else if (maxChild == child4) maxChild = 4;
+		
+		System.out.println("Average: " + (children / T) + " children to get at least one of each gender.");
+		System.out.println("Number of families with 2 children: " + child2);
+		System.out.println("Number of families with 3 children: " + child3);
+		System.out.println("Number of families with 4 children: " + child4);
+		System.out.println("The most common number of children is " + maxChild + ".");
 	}
 }
